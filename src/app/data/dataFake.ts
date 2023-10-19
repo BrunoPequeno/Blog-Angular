60d9321405e3d8e12a059cfbf9ba1e97e2964017
Bruno Pequeno <bru.pequeno@gmail.com>
2023-10-19 00:19:11 -0300

    first commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "ESTREIA EM 3D",
+    "description": "A gigante Marvel anuncia um novo filme do homem de ferro em 3D, que promete chocar o mundo com a veracidade usadas nas cenas, confira agora mesmo",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"2",
+    "title": "Nova Série anunciada no Disney +",
+    "description": "Disney + anuncia lançamento de série imperdível em parceria com a Marvel",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"3",
+    "title":"Suspense de estreia",
+    "description":"Ontem em entrevista, criador do pantera negra anuncia que logo teremos novidades, deixando um ar de suspense para um novo filme",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/35720ACD323F927C3A83D809D0F460BD3651740DA519BCC184B6F042057EA14D/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"4",
+    "title":"Filme em pós produção",
+    "description":"Ontem em entrevista, criador do X-Men anuncia que filme está em pós produção e logo vai aos cartazes",
+    "photoCover":"https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339"
+  }
+]
